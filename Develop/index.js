282b0016ca1aaf1659c474bbe0e60db245c0133c
Jordan Ross <jorross@umich.edu>
2022-03-03 19:27:38 -0800

    got a readMe file generating properly

--- Develop/index.js
+++ Develop/index.js
@@ -4,10 +4,12 @@ const fs = require('fs');
 const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = ["What's your name?", "Who's your daddy?", "Does he look like me?"];
+const questions = ["What's your name?", "What's the title of your project?", "What does your project do?", "How does a user install your project?", "Describe the usage of your project", "Which of the following licenses would you like to include?", "How does someone contribute to the project?", "How does a user run the test suite?", "What is your github profile name?", "What is your email address?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile(fileName, data, (err) => err ? console.error(err) : console.log("Write to file succeeded"));
+}
 
 var i = 0;
 
@@ -21,24 +23,113 @@ function init() {
         },
         {
             type: "input",
-            name: "daddy",
+            name: "title",
             message: questions[i++]
         },
         {
             type: "input",
-            name: "me",
+            name: "description",
+            message: questions[i++]
+        },
+        {
+            type: "input",
+            name: "install",
+            message: questions[i++]
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: questions[i++]
+        },
+        {
+            type: "rawlist",
+            name: "license",
+            message: questions[i++],
+            choices: ["MIT License", "The Unlicense"]
+        },
+        {
+            type: "input",
+            name: "contribute",
+            message: questions[i++]
+        },
+        {
+            type: "input",
+            name: "test",
+            message: questions[i++]
+        },
+        {
+            type: "input",
+            name: "github",
+            message: questions[i++]
+        },
+        {
+            type: "input",
+            name: "email",
             message: questions[i++]
         },
     ])
-    .then((data) => {
-        const filename = "README.md";
-        console.log(data);
-        // const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-    
-        // fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-        //   err ? console.log(err) : console.log('Success!')
-        // );
-      });
+        .then((data) => {
+            const filename = "README.md";
+            console.log(data);
+            if (data.license === "MIT License") {
+                data.license = `MIT License
+
+            Copyright (c) [year] [fullname]
+            
+            Permission is hereby granted, free of charge, to any person obtaining a copy
+            of this software and associated documentation files (the "Software"), to deal
+            in the Software without restriction, including without limitation the rights
+            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+            copies of the Software, and to permit persons to whom the Software is
+            furnished to do so, subject to the following conditions:
+            
+            The above copyright notice and this permission notice shall be included in all
+            copies or substantial portions of the Software.
+            
+            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+            SOFTWARE.
+            `;
+            }
+            else {
+                data.license = `
+            This is free and unencumbered software released into the public domain.
+
+            Anyone is free to copy, modify, publish, use, compile, sell, or
+            distribute this software, either in source code form or as a compiled
+            binary, for any purpose, commercial or non-commercial, and by any
+            means.
+
+            In jurisdictions that recognize copyright laws, the author or authors
+            of this software dedicate any and all copyright interest in the
+            software to the public domain. We make this dedication for the benefit
+            of the public at large and to the detriment of our heirs and
+            successors. We intend this dedication to be an overt act of
+            relinquishment in perpetuity of all present and future rights to this
+            software under copyright law.
+
+            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+            EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+            MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+            IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+            OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+            ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+            OTHER DEALINGS IN THE SOFTWARE.
+
+            For more information, please refer to <https://unlicense.org>
+            `;
+            }
+            writeToFile(filename, generateMarkdown(data));
+            // const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+            // fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+            //   err ? console.log(err) : console.log('Success!')
+            // );
+        });
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,63 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
 
 `;
 }
