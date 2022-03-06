1541bc25067e24b94d5333d7112a9aedec016c2d
Jordan Ross <jorross@umich.edu>
2022-03-05 20:48:34 -0800

    just about finished

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,25 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require("./utils/generateMarkdown")
+const generateMarkdown = require("./utils/generateMarkdown");
+// const renderLicenseBadge = require("./utils/generateMarkdown");
+// const renderLicenseLink = require("./utils/generateMarkdown");
+// const renderLicenseSection = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = ["What's your name?", "What's the title of your project?", "What does your project do?", "How does a user install your project?", "Describe the usage of your project", "Which of the following licenses would you like to include?", "How does someone contribute to the project?", "How does a user run the test suite?", "What is your github profile name?", "What is your email address?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.appendFile(fileName, data, (err) => err ? console.error(err) : console.log("Write to file succeeded"));
+    fs.writeFile(fileName, '', (err) => {
+        if (err) {
+            console.error(err);
+        }
+        else{
+            console.log("Cleared file succeeded");
+            fs.writeFile(fileName, data, (err) => err ? console.error(err) : console.log("Write to file succeeded"));
+        }
+    });
 }
 
 var i = 0;
@@ -70,65 +81,13 @@ function init() {
     ])
         .then((data) => {
             const filename = "README.md";
-            
-            if (data.license === "MIT License") {
-                data.license = `MIT License
+            data.badge = generateMarkdown.renderLicenseBadge(data.license);
+            data.link = generateMarkdown.renderLicenseLink(data.license);
+            data.license = generateMarkdown.renderLicenseSection(data.license);
 
-                Copyright (c) 2022 ${data.name}
-                
-                Permission is hereby granted, free of charge, to any person obtaining a copy
-                of this software and associated documentation files (the "Software"), to deal
-                in the Software without restriction, including without limitation the rights
-                to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                copies of the Software, and to permit persons to whom the Software is
-                furnished to do so, subject to the following conditions:
-                
-                The above copyright notice and this permission notice shall be included in all
-                copies or substantial portions of the Software.
-                
-                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-                IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-                FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-                AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-                LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-                OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-                SOFTWARE.
-                `;
+            console.log(data);
 
-                data.badge = `[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)`;
-
-            }
-            else {
-                data.license = `
-                This is free and unencumbered software released into the public domain.
-
-                Anyone is free to copy, modify, publish, use, compile, sell, or
-                distribute this software, either in source code form or as a compiled
-                binary, for any purpose, commercial or non-commercial, and by any
-                means.
-
-                In jurisdictions that recognize copyright laws, the author or authors
-                of this software dedicate any and all copyright interest in the
-                software to the public domain. We make this dedication for the benefit
-                of the public at large and to the detriment of our heirs and
-                successors. We intend this dedication to be an overt act of
-                relinquishment in perpetuity of all present and future rights to this
-                software under copyright law.
-
-                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-                EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-                MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-                IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-                OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-                ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-                OTHER DEALINGS IN THE SOFTWARE.
-
-                For more information, please refer to <https://unlicense.org>
-                `;
-                data.badge = `[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)`;
-            }
-
-            writeToFile(filename, generateMarkdown(data));
+            writeToFile(filename, generateMarkdown.generateMarkdown(data));
         });
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,102 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+  if (license === "MIT License") {
+    return `[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)`;
+
+  }
+  else {
+    return `[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+  if (license == "MIT License") {
+    return `https://opensource.org/licenses/MIT
+    `;
+
+    // data.badge = `[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)`;
+
+  }
+  else {
+    return `
+    https://unlicense.org
+    `;
+    // data.badge = `[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+  if (license === "MIT License") {
+    return `MIT License
+
+    Copyright (c) 2022
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    `;
+
+    // data.badge = `[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)`;
+
+  }
+  else {
+    return `
+    This is free and unencumbered software released into the public domain.
+
+    Anyone is free to copy, modify, publish, use, compile, sell, or
+    distribute this software, either in source code form or as a compiled
+    binary, for any purpose, commercial or non-commercial, and by any
+    means.
+
+    In jurisdictions that recognize copyright laws, the author or authors
+    of this software dedicate any and all copyright interest in the
+    software to the public domain. We make this dedication for the benefit
+    of the public at large and to the detriment of our heirs and
+    successors. We intend this dedication to be an overt act of
+    relinquishment in perpetuity of all present and future rights to this
+    software under copyright law.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+
+    For more information, please refer to <https://unlicense.org>
+    `;
+    // data.badge = `[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -59,4 +147,7 @@ ${data.email}
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports.generateMarkdown = generateMarkdown;
+module.exports.renderLicenseBadge = renderLicenseBadge;
+module.exports.renderLicenseLink = renderLicenseLink;
+module.exports.renderLicenseSection = renderLicenseSection;
